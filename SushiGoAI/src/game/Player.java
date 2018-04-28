196e70614bc4f14408ed6201efbae98da7000e67
Tara Iyer <tiyer@stanford.edu>
2018-04-28 15:34:27 -0700

    Updated Player class with the following:
    - Instance variables for cards (ArrayList<String>), totalPoints (int),
    and numPuddings (int). Added associated getters and setters for each
    variable.
    
    - constructor for the class.

--- SushiGoAI/src/game/Player.java
+++ SushiGoAI/src/game/Player.java
@@ -1,5 +1,40 @@
 package game;
 
-public class Player {
+import java.util.*;
 
-}
+public class Player {
+	
+	private ArrayList<String> cards;
+	private int totalPoints;
+	private int numPuddings;
+	
+	public Player() {
+		cards = new ArrayList<String>();
+		totalPoints = 0;
+		numPuddings = 0;
+	}
+	
+	public void updateCards(String newCard) {
+		cards.add(newCard);
+	}
+	
+	public ArrayList<String> getCards() {
+		return cards;
+	}
+	
+	public void updateTotalPoints(int pointsToAdd) {
+		totalPoints += pointsToAdd;
+	}
+	
+	public int getTotalPoints() {
+		return totalPoints;
+	}
+	
+	public void updateNumPuddings(int puddingsToAdd) {
+		numPuddings += puddingsToAdd;
+	}
+	
+	public int getNumPuddings() {
+		return numPuddings;
+	}
+ }
