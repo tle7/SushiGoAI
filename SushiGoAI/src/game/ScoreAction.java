e2c1c60f045c4f7b4597615d301a68fcba30c51b
tle7 <timothyle7@gmail.com>
2018-05-24 15:31:03 -0700

    fixed ScoreAction

---
+++ SushiGoAI/src/game/ScoreAction.java
@@ -0,0 +1,14 @@
+package game;
+
+import java.util.*;
+
+public class ScoreAction {
+
+	public ArrayList<String> actions;
+	public int score;
+	
+	public ScoreAction() {
+		actions = new ArrayList<String>();
+		score = 0;
+	}
+}
